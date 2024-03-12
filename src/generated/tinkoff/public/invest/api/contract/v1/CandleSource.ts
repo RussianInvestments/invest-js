fbf002f120dc1fd17683c669fa448777438af95a
s.radyukov <s.radyukov@tinkoff.ru>
2024-03-12 13:54:53 +0300

    Release v1.19

---
+++ src/generated/tinkoff/public/invest/api/contract/v1/CandleSource.ts
@@ -0,0 +1,7 @@
+// Original file: investAPI/src/docs/contracts/marketdata.proto
+
+export enum CandleSource {
+  CANDLE_SOURCE_UNSPECIFIED = 0,
+  CANDLE_SOURCE_EXCHANGE = 1,
+  CANDLE_SOURCE_DEALER_WEEKEND = 2,
+}
